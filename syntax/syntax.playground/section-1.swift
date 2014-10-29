bbf277a91ff49ad89c78ccf620aaf1ea4c9a2814
Alexander Dobrynin <adobryni@pip07.inf.fh-koeln.de>
2014-10-29 12:41:03 +0100

    syntax verzeichnis mit beispiel

---
+++ syntax/syntax.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+
+let emptyArray = [String](shoppingList)
+//let emptyDictionary = [String: String](occupations)
