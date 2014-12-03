9f25a242e71cf7da0f1de11f892fb8432abc9b84
Stefan Heruth <sh056013@pip04.inf.fh-koeln.de>
2014-12-03 15:22:09 +0100

    add call by reference in Objective C

---
+++ sprachkonstrukte/call by reference/CallByReference/CallByReference/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CallByReference
+//
+//  Created by Stefan Heruth on 03.12.14.
+//  Copyright (c) 2014 Stefan Heruth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
